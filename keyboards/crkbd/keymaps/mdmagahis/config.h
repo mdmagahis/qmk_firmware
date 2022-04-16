90eaf7533e05c55daf9dcf0f2efc339671ed5fee
Mark Magahis <mark.magahis@gmail.com>
2022-04-16 13:20:50 -0500

    mdmagahis keymap v0.1.3
    - Adds home row mods and dynamic tapping term
    - Adds support for Keyboard Maestro macros top/bottom-thirds of screen
    - move to magahis.json with contained versioning
    
    TODO:
    - [ ] prune unwanted RGB matrix effects
    - [ ] remove unnecessary oled references

--- keyboards/crkbd/keymaps/mdmagahis/config.h
+++ keyboards/crkbd/keymaps/mdmagahis/config.h
@@ -28,8 +28,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-//#define TAPPING_FORCE_HOLD
-//#define TAPPING_TERM 100
+#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_FORCE_HOLD
+#define TAPPING_TERM 200
 
 // #ifdef RGBLIGHT_ENABLE
 //     #define RGBLIGHT_EFFECT_BREATHING
