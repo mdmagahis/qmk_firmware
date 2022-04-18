fef5fb16969a3f624b89f9ff2bf316612714b5b0
Mark Magahis <mark.magahis@gmail.com>
2022-04-17 21:12:53 -0500

    mdmagahis keymap v0.2.0
    - space saving optimizations
     - prune unwanted RGB matrix effects
     - remove unnecessary oled references

--- keyboards/crkbd/keymaps/mdmagahis/rules.mk
+++ keyboards/crkbd/keymaps/mdmagahis/rules.mk
@@ -2,6 +2,5 @@ MOUSEKEY_ENABLE             = yes    # Mouse keys
 RGBLIGHT_ENABLE             = no
 RGB_MATRIX_ENABLE           = yes
 OLED_ENABLE                 = no
-OLED_DRIVER                 = SSD1306
 LTO_ENABLE                  = yes
 DYNAMIC_TAPPING_TERM_ENABLE = yes
