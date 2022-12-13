962e2c269f4c99f749346ccbf1993ae362596630
Mark Magahis <mark.magahis@gmail.com>
2022-12-13 09:57:01 -0500

    mdmagahis keymap v0.4.6
     - swith KC_O and KC_U

--- keyboards/crkbd/keymaps/mdmagahis/keymap.c
+++ keyboards/crkbd/keymaps/mdmagahis/keymap.c
@@ -28,7 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define HOME_SL RSFT_T(KC_SLSH)
 #define HOME_E RGUI_T(KC_E)
 #define HOME_I LALT_T(KC_I)
-#define HOME_O RCTL_T(KC_O)
+#define HOME_U RCTL_T(KC_U)
 
 // Tab Navigation
 #define TAB_P SGUI(KC_LBRC)
@@ -80,9 +80,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------------.                 ,-----------------------------------------------------------.
-             KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,   KC_L,    KC_U,    KC_Y, KC_MINS,        KC_BSPC,
+             KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,   KC_L,    KC_O,    KC_Y, KC_MINS,        KC_BSPC,
   //|--------------+--------+--------+--------+--------+--------|                 |--------+-------+--------+--------+--------+---------------|
-      ALL_T(KC_ESC),  HOME_A,  HOME_R,  HOME_S,  HOME_T,    KC_G,                      KC_M, HOME_N,  HOME_E,  HOME_I,  HOME_O, ALL_T(KC_QUOT),
+      ALL_T(KC_ESC),  HOME_A,  HOME_R,  HOME_S,  HOME_T,    KC_G,                      KC_M, HOME_N,  HOME_E,  HOME_I,  HOME_U, ALL_T(KC_QUOT),
   //|--------------+--------+--------+--------+--------+--------|                 |--------+-------+--------+--------+--------+---------------|
             KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                      KC_K,   KC_H, KC_COMM, KC_DOT,  KC_SLSH,        KC_SCLN,
   //|--------------+--------+--------+--------+--------+--------+-------|  |------+--------+-------+--------+--------+--------+---------------|
