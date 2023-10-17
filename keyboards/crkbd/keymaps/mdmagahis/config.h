128469d7b52ae72a74fbdf8e5b22370aa8667f28
Mark Magahis <mark.magahis@gmail.com>
2023-10-17 09:39:21 -0400

    mdmagahis keymap v0.4.10
        - add macros to left thumbs

--- keyboards/crkbd/keymaps/mdmagahis/config.h
+++ keyboards/crkbd/keymaps/mdmagahis/config.h
@@ -30,7 +30,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
 
-#define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 150
 
